16d3a4b2c5075924fb8ab838fe9518dbe170426a
Kosta Dunn <kostacoffee@gmail.com>
2016-12-21 20:45:50 +1100

    Added an `Add Event` dialog

---
+++ src/components/AddEventDialog.vue
@@ -0,0 +1,73 @@
+<template lang="pug">
+md-dialog(ref="add-event-dialog")
+	form(v-on:submit.prevent="addEvent")
+		md-dialog-title
+			h1.md-title Add Event
+
+		md-dialog-content
+			md-input-container
+				label Event Title
+				md-input(v-model="title")
+
+			md-input-container
+				label Additional Info
+				md-input(v-model="desc")
+
+			md-input-container
+				label When is it on?
+				md-input(type="datetime", v-model="time")
+
+		md-dialog-actions
+			md-button(type="submit") Add Event
+			md-button(v-on:click="closeDialog('add-event-dialog')") Close
+</template>
+
+<script>
+export default {
+	name: 'add-event-dialog',
+	data () {
+		return {
+			title: null,
+			desc: null,
+			time: null
+		}
+	},
+	methods: {
+		addEvent: async function () {
+			let data = {
+				title: this.title,
+				desc: this.desc,
+				time: this.time
+			};
+
+			let resp = null;
+
+			try {
+				resp = await this.$http.post('/events', data);
+			}
+			catch (e) {
+				resp = null;
+			}
+
+			if (resp == null)
+				return;
+			
+			//reset fields
+			this.title = null;
+			this.desc = null;
+			this.time = null;
+
+			this.closeDialog('add-event-dialog');
+		},
+
+		closeDialog: function (ref) {
+			this.$refs[ref].close();
+		}
+	}
+}
+
+</script>
+
+<style lang="sass">
+
+</style>
--- src/views/EventMenu.vue
+++ src/views/EventMenu.vue
@@ -2,9 +2,10 @@
 div#event-menu
 	md-whiteframe(md-tag="md-toolbar")
 		h1.md-title SUITS Events
-		md-button.md-fab.md-accent.new-event-btn.md-raised
+		md-button.md-fab.md-accent.new-event-btn.md-raised(v-on:click="openDialog('add-event-dialog')")
 			md-icon add
 			md-tooltip(direction="bottom") Add Event
+	add-event-dialog(ref="add-event-dialog")
 	event-list(:socket="socket")
 </template>
 
@@ -15,12 +16,15 @@ export default {
 	name: "event-menu",
 	data () {
 		return {
-			socket: null,
+			socket: null
 		}
 	},
 	components: {
 		'event-list': function(resolve) {
 			require(['../components/EventList.vue'], resolve)
+		},
+		'add-event-dialog': function(resolve) {
+			require(['../components/AddEventDialog.vue'], resolve)
 		}
 	},
 	mounted: function() {
@@ -29,6 +33,11 @@ export default {
 		this.socket = io(config.apiURL, {
 			query: {token: token}
 		});
+	},
+	methods: {
+		openDialog: function (ref) {
+			this.$refs[ref].$children[0].open() //oh god pls help me
+		}
 	}
 }
 
